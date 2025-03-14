893b6f6718421b3804d2d4a5fe3f4fa3c90b2fad
lashtheone <jbgsn0214@proton.me>
2025-03-14 14:04:36 +0800

    建立了基础项目，链接了Sandbox和DreamEngine

---
+++ DreamEngine/src/test.cpp
@@ -0,0 +1,10 @@
+#include "test.h"
+#include <stdio.h>
+
+namespace DreamEngine {
+
+	void Print()
+	{
+		printf("Welcome to DreamEngine!\n");
+	}
+}
---
+++ Sandbox/src/APP.cpp
@@ -0,0 +1,8 @@
+
+namespace DreamEngine {
+	__declspec(dllimport) void Print();
+}
+
+void main() {
+	DreamEngine::Print();
+}
