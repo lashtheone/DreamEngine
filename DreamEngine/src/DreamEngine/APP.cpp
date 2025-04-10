f5f9b2de654b2768683c2504f1e8a1d4efd3225c
lashtheone <jbgsn0214@proton.me>
2025-04-10 14:23:25 +0800

    Window Event

--- DreamEngine/src/DreamEngine/APP.cpp
+++ DreamEngine/src/DreamEngine/APP.cpp
@@ -1,17 +1,26 @@
 #include "depch.h"
 #include "APP.h"
 
-#include "DreamEngine/Events/APPEvent.h"
 #include "DreamEngine/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace DreamEngine {
+
+#define BIND_EVENT_FN(x) std::bind(&APP::x, this, std::placeholders::_1)
+
 	APP::APP() {
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 	APP::~APP() {
 
+	}
+
+	void APP::OnEvent(Event& e) {
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		DE_CORE_TRACE("{0}", e);
 	}
 
 	void APP::Run() {
@@ -21,5 +30,10 @@ namespace DreamEngine {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool APP::OnWindowClose(WindowCloseEvent& e) {
+		m_Running = false;
+		return true;
 	}
 }
--- DreamEngine/src/DreamEngine/APP.h
+++ DreamEngine/src/DreamEngine/APP.h
@@ -3,6 +3,8 @@
 #include "Core.h"
 #include "Events/Event.h"
 #include "Window.h"
+#include "DreamEngine/Events/APPEvent.h"
+
 
 namespace DreamEngine {
 	class DREAM_API APP
@@ -13,7 +15,10 @@ namespace DreamEngine {
 
 		void Run();
 
+		void OnEvent(Event& e);
+
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
